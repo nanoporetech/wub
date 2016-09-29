14c1cf1567b27870a3f6dc2ecd8a0e2b29ccc73f
Botond Sipos <bsipos@nanoporetech.com>
2016-09-29 15:47:10 +0100

    fasta_to_mock_fastq: simplified!

--- scripts/fasta_to_mock_fastq.py
+++ scripts/fasta_to_mock_fastq.py
@@ -13,12 +13,11 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     '-q', metavar='mock_quals', type=int, help="Mock quality value (40).", default=40)
 
-def add_qualities(input_iter, mock_qual):
-    for record in input_iter:
-        yield seq.mock_qualities(record, mock_qual)
-
 if __name__ == '__main__':
     args = parser.parse_args()
 
+    mock_qual = args.q
+
     input_iterator = SeqIO.parse(sys.stdin, 'fasta')
-    SeqIO.write(add_qualities(input_iterator, args.q), sys.stdout, 'fastq')
+    output_iterator = (seq.mock_qualities(record, mock_qual) for record in input_iterator)
+    SeqIO.write(output_iterator, sys.stdout, 'fastq')
