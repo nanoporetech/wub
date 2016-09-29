f2894730447de74952d8d3b38c051808faccf43d
Botond Sipos <bsipos@nanoporetech.com>
2016-09-29 17:56:17 +0100

    Working on sequence simulation.

--- wub/simulate/genome.py
+++ wub/simulate/genome.py
@@ -2,3 +2,5 @@
 
 import numpy as np
 
+from wub.simulate import seq as sim_seq
+
--- wub/simulate/seq.py
+++ wub/simulate/seq.py
@@ -2,3 +2,14 @@
 
 import numpy as np
 
+from wub.util import seq as seq_util
+
+uniform = [0.25, 0.25, 0.25, 0.25]
+
+def random_base(probs=uniform):
+    return np.random.choice(seq_util.bases, p=probs)
+
+def random_base_except(excluded, probs=uniform):
+    # In progress. Must re-normalise probabilities.
+    filtered_bases = seq_util.bases[:].remove(excluded)
+    return np.random.choice(filtered_bases)
