6c8b3ca8e405c14a0f4b48cbb3389c2545787d38
Botond Sipos <bsipos@nanoporetech.com>
2016-09-30 10:55:40 +0100

    Genome simulation tool now uses base frequencies.

--- scripts/simulate_genome.py
+++ scripts/simulate_genome.py
@@ -4,16 +4,36 @@
 import argparse
 import sys
 
+from Bio import SeqIO
+import numpy as np
+
+from wub.simulate import genome as sim_genome
+from wub.util import parse as parse_util
+
 # Parse command line arguments:
 parser = argparse.ArgumentParser(
     description='Simulate genome sequence with the specified number of chromosomes, length distribution (truncated gamma) and base composition. Output is fasta to the standard output.')
-parser.add_argument('-n', metavar='nr_chrom', type=int, help="Number of chromosomes (23).", default=23)
-parser.add_argument('-m', metavar='mean_length', type=int, help="Mean length of chromosomes (50000000).", default=50000000)
-parser.add_argument('-a', metavar='gamma_shape', type=float, help="Gamma shape parameter (1).", default=1.0)
-parser.add_argument('-l', metavar='low_trunc', type=int, help="Lower truncation point (None).", default=None)
-parser.add_argument('-u', metavar='high_trunc', type=int, help="Upper truncation point (None).", default=None)
-parser.add_argument('-b', metavar='base_freqs', type=str, help="Relative base frequencies in A,C,G,T order (1,1,1,1).", default="1,1,1,1")
+parser.add_argument(
+    '-n', metavar='nr_chrom', type=int, help="Number of chromosomes (23).", default=23)
+parser.add_argument('-m', metavar='mean_length', type=int,
+                    help="Mean length of chromosomes (5000000).", default=5000000)
+parser.add_argument(
+    '-a', metavar='gamma_shape', type=float, help="Gamma shape parameter (1).", default=1.0)
+parser.add_argument(
+    '-l', metavar='low_trunc', type=int, help="Lower truncation point (None).", default=None)
+parser.add_argument(
+    '-u', metavar='high_trunc', type=int, help="Upper truncation point (None).", default=None)
+parser.add_argument('-b', metavar='base_freqs', type=str,
+                    help="Relative base frequencies in A,C,G,T order (1,1,1,1).", default="1,1,1,1")
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
+
+    base_frequencies = np.array(parse_util.separated_list_to_floats(args.b))
+    # Normalise relative base frequencies to probabilities:
+    base_frequencies = base_frequencies / np.sum(base_frequencies)
+
+    simulation_iterator = sim_genome.simulate_genome(
+        args.n, args.m, args.a, args.l, args.u, base_frequencies)
+    SeqIO.write(simulation_iterator, sys.stdout, 'fasta')
--- wub/simulate/genome.py
+++ wub/simulate/genome.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from collections import OrderedDict
 
+from wub.util import seq as seq_util
 from wub.simulate import seq as sim_seq
+from wub.simulate import dist
 
+
+def simulate_genome(number_chromosomes, mean_length, gamma_shape, low_truncation, high_truncation, base_frequencies):
+    chrom_info = OrderedDict(
+        ('chr' + str(i), int(dist.sample_truncated_gamma(mean_length, gamma_shape, low_truncation, high_truncation))) for i in xrange(number_chromosomes))
+    sim_iter = (seq_util.new_dna_record(sim_seq.simulate_sequence(length, base_frequencies), name) for name, length in chrom_info.iteritems())
+    return sim_iter
