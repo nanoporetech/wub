88c2f0e1bb4b79604e916553390b65566eb9f252
Botond Sipos <bsipos@nanoporetech.com>
2016-09-30 11:54:23 +0100

    Documented simulate.genome

--- wub/simulate/genome.py
+++ wub/simulate/genome.py
@@ -9,6 +9,18 @@ from wub.simulate import dist
 
 
 def simulate_genome(number_chromosomes, mean_length, gamma_shape, low_truncation, high_truncation, base_frequencies):
+    """Generator function for simulating chromosomes in a genome. Chromosome lengths are sampled from a truncated gamma distribution.
+
+    :param number_chromosomes: Number of simulated chromosomes.
+    :param mean_length: Mean length of simulated chromosomes.
+    :param gamma_shape: Shape parameter of the chromosome length distribution.
+    :param low_truncation: Minimum chromosome length.
+    :param high_truncation: Maximum chromosome length.
+    :param base_frequencies: Comma-separated list of relative base frequencies in the ACGT order.
+    :returns: A generator of SeqRecord objects.
+    :rtype: generator
+
+    """
     chrom_info = OrderedDict(
         ('chr' + str(i), int(dist.sample_truncated_gamma(mean_length, gamma_shape, low_truncation, high_truncation))) for i in xrange(number_chromosomes))
     sim_iter = (seq_util.new_dna_record(sim_seq.simulate_sequence(length, base_frequencies), name) for name, length in chrom_info.iteritems())
