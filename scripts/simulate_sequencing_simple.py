4ad5539a8f4778a61560b9792604e90550d0b589
Botond Sipos <bsipos@nanoporetech.com>
2016-10-03 15:48:56 +0100

    simulate_sequencing_simple: added doc and comments.

--- scripts/simulate_sequencing_simple.py
+++ scripts/simulate_sequencing_simple.py
@@ -14,7 +14,12 @@ from wub.util import seq as seq_util
 
 # Parse command line arguments:
 parser = argparse.ArgumentParser(
-    description='Sample fragments from the input genome and simulate sequencing errors. Read lengths are drawn from the specified truncated gamma distribution. Chromosomes are sampled randomly for each read.')
+    description="""Sample fragments from the input genome and simulate sequencing errors. Read lengths are drawn from the specified truncated gamma distribution. Chromosomes are sampled randomly for each read.
+    
+    The format of the read names is the following:
+    r<unique_id>_<chromosome>_<frag_start>_<frag_end>/q<realised_quality>/s<realised_substiutions>/d<realised_deletions>/i<relaised_insertions>
+    
+    """)
 parser.add_argument(
     '-n', metavar='nr_reads', type=int, help="Number of simulated reads (1).", default=1)
 parser.add_argument('-m', metavar='mean_length', type=int,
@@ -37,11 +42,42 @@ parser.add_argument('output_fastq', nargs='?', help='Output fastq (default: stdo
                     type=argparse.FileType('w'), default=sys.stdout)
 
 
+def simulate_sequencing(chromosomes, mean_length, gamma_shape, low_truncation, high_truncation, mock_quality, number_reads):
+    """Simulate sequenceing.
+    :param chromosomes: Input chromosomes (list of SeqRecord obejcts).
+    :param mean_length: Mean read length.
+    :param gamma_shape: Shape paramter of the read length distribution.
+    :param low_truncation: Minimum read length.
+    :param high_truncation: Maximum read length.
+    :param mock_quality: Mock base quality for fastq output.
+    :param number_reads: Number of reads to simulate.
+    """
+    for fragment in sim_genome.simulate_fragments(chromosomes, mean_length, gamma_shape, low_truncation, high_truncation, number_reads):
+        # Simulate sequenceing errors:
+        mutated_record = sim_seq.simulate_sequencing_errors(
+            fragment.seq, args.e, error_weights)
+        # Construct read name:
+        read_name = "r{}_{}_{}_{}".format(
+            fragment.uid, fragment.chrom, fragment.start, fragment.end)
+        read_name = "{}/q{}/s{}/d{}/i{}".format(read_name, mutated_record.real_qual, mutated_record.real_subst, mutated_record.real_del, mutated_record.real_ins)
+
+        yield seq_util.new_dna_record(mutated_record.seq, read_name, [mock_quality] * len(mutated_record.seq))
+
 if __name__ == '__main__':
     args = parser.parse_args()
 
     # Read in chromosomes of the input genome:
     chromosomes = list(seq_util.read_seq_records(args.input_fasta))
 
-    for fragment in sim_genome.simulate_fragments(chromosomes, args.m, args.a, args.l, args.u, args.n):
-        print fragment
+    # Process error weights:
+    error_weights = np.array(parse_util.separated_list_to_floats(args.w))
+    # Normalise relative base frequencies to probabilities:
+    error_weights = parse_util.normalise_array(error_weights)
+    error_weights = dict(
+        zip(['substitution', 'insertion', 'deletion'], error_weights))
+
+    simulation_iterator = simulate_sequencing(
+        chromosomes, args.m, args.a, args.l, args.u, args.q, args.n)
+
+    seq_util.write_seq_records(
+        simulation_iterator, args.output_fastq, format='fastq')
