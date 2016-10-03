2272c9c1f3e23b980e703edd3214e9821b59d6b2
Botond Sipos <bsipos@nanoporetech.com>
2016-10-03 09:06:42 +0100

    utils.seq: added utility functions for reading and parsing sequence files.

--- wub/util/seq.py
+++ wub/util/seq.py
@@ -27,6 +27,7 @@ def mock_qualities(record, mock_qual):
     rec_copy.letter_annotations["phred_quality"] = [mock_qual] * len(rec_copy)
     return rec_copy
 
+
 def new_dna_record(sequence, name, qualities=None):
     """Create a new SeqRecord object using IUPACUnambiguousDNA and the specified sequence.
 
@@ -42,15 +43,34 @@ def new_dna_record(sequence, name, qualities=None):
         seq_record.letter_annotations["phred_quality"] = qualities
     return seq_record
 
-def write_seq_records(records_iterator, output_file, format='fasta'):
+
+def write_seq_records(records_iterator, output_object, format='fasta'):
     """Write out SeqRecord objects to a file from an iterator in the specified format.
 
     :param records_iterator: An iterator of SeqRecord objects.
-    :param output_file: Output file.
+    :param output_object: Open file object or file name.
     :param format: Output format (fasta by default).
     :returns: None
     :rtype: object
 
     """
-    with open(output_file, 'w') as output_handle:
-        SeqIO.write(records_iterator, output_handle, format)
+    if type(output_object) == file:
+        SeqIO.write(records_iterator, output_object, format)
+    else:
+        with open(output_file, 'w') as output_handle:
+            SeqIO.write(records_iterator, output_handle, format)
+
+
+def read_seq_records(input_object, format='fasta'):
+    """Write out SeqRecord objects to a file from an iterator in the specified format.
+
+    :param input_object: A file object or a file name.
+    :param format: Input format (fasta by default).
+    :returns: An iterator of SeqRecord objects.
+    :rtype: generator
+
+    """
+    handle = input_object
+    if type(handle) != file:
+        handle = open(handle, "rU")
+    return SeqIO.parse(handle, format)
