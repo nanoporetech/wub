97108c4edfcff7aa47aef1d7890b41475b3a6462
Botond Sipos <bsipos@nanoporetech.com>
2016-10-03 09:51:28 +0100

    Implemented simulation of fragments from a set of chromosomes.

--- scripts/simulate_sequencing_simple.py
+++ scripts/simulate_sequencing_simple.py
@@ -8,11 +8,13 @@ from Bio import SeqIO
 import numpy as np
 
 from wub.simulate import seq as sim_seq
+from wub.simulate import genome as sim_genome
 from wub.util import parse as parse_util
+from wub.util import seq as seq_util
 
 # Parse command line arguments:
 parser = argparse.ArgumentParser(
-    description='Sample fragments from the input genome and simulate sequencing errors.')
+    description='Sample fragments from the input genome and simulate sequencing errors. Read lengths are drawn from the specified truncated gamma distribution. Chromosomes are sampled randomly for each read.')
 parser.add_argument(
     '-n', metavar='nr_reads', type=int, help="Number of simulated reads (1).", default=1)
 parser.add_argument('-m', metavar='mean_length', type=int,
@@ -22,15 +24,24 @@ parser.add_argument(
 parser.add_argument(
     '-l', metavar='low_trunc', type=int, help="Read length distribution: lower truncation point (None).", default=None)
 parser.add_argument(
+    '-u', metavar='high_trunc', type=int, help="Read length distribution: upper truncation point (None).", default=None)
+parser.add_argument(
     '-e', metavar='error_rate', type=float, help="Total rate of substitutions insertions and deletions (0.1).", default=0.1)
 parser.add_argument('-w', metavar='error_weights', type=str,
                     help="Relative frequency of substitutions,insertions,deletions (1,1,4).", default="1,1,4")
-parser.add_argument('input_fasta', nargs='?', help='Input genome in fasta format (default: stdin).', type=argparse.FileType('r'), default=sys.stdin)
-parser.add_argument('output_fastq', nargs='?', help='Output fastq (default: stdout)', type=argparse.FileType('w'), default=sys.stdout)
+parser.add_argument(
+    '-q', metavar='mock_quality', type=int, help="Mock base quality for fastq output (40).", default=40)
+parser.add_argument('input_fasta', nargs='?', help='Input genome in fasta format (default: stdin).',
+                    type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('output_fastq', nargs='?', help='Output fastq (default: stdout)',
+                    type=argparse.FileType('w'), default=sys.stdout)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    for line in args.input_fasta:
-        print line
+    # Read in chromosomes of the input genome:
+    chromosomes = list(seq_util.read_seq_records(args.input_fasta))
+
+    for fragment in sim_genome.simulate_fragments(chromosomes, args.m, args.a, args.l, args.u, args.n):
+        print fragment
--- wub/simulate/genome.py
+++ wub/simulate/genome.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 
 from wub.util import seq as seq_util
 from wub.simulate import seq as sim_seq
@@ -16,12 +16,70 @@ def simulate_genome(number_chromosomes, mean_length, gamma_shape, low_truncation
     :param gamma_shape: Shape parameter of the chromosome length distribution.
     :param low_truncation: Minimum chromosome length.
     :param high_truncation: Maximum chromosome length.
-    :param base_frequencies: Comma-separated list of relative base frequencies in the ACGT order.
+    :param base_frequencies: Array of base frequencies in the ACGT order.
     :returns: A generator of SeqRecord objects.
     :rtype: generator
 
     """
     chrom_info = OrderedDict(
         ('chr' + str(i), int(dist.sample_truncated_gamma(mean_length, gamma_shape, low_truncation, high_truncation))) for i in xrange(number_chromosomes))
-    sim_iter = (seq_util.new_dna_record(sim_seq.simulate_sequence(length, base_frequencies), name) for name, length in chrom_info.iteritems())
+    sim_iter = (seq_util.new_dna_record(sim_seq.simulate_sequence(length, base_frequencies), name)
+                for name, length in chrom_info.iteritems())
     return sim_iter
+
+
+def sample_chromosome(chromosomes):
+    """Sample a random chromosome.
+
+    :param chromosomes: A collection of SeqRecord object.
+    :returns: A randomly sampled element from the input collection.
+    :rtype: SeqRecord
+    """
+    indexes = range(len(chromosomes))
+    pick = np.random.choice(indexes)
+    return chromosomes[pick]
+
+
+def simulate_fragment(chromosome, mean_length, gamma_shape, low_truncation, high_truncation, fragment_number):
+    """Simulate a fragment from a chromosome.
+
+    :param chromosome: Chromosome to simulate fragment from, SeqRecord object.
+    :param mean_length: Mean length of simulated fragment.
+    :param gamma_shape: Shape parameter of length distribution.
+    :param low_truncation: Minimum read length.
+    :param high_truncation: Maximum read length.
+    :param fragment_number: The unique identifier of fragment in simulation (number of fragment).
+    :returns: A named tuple with chromosome id, fragment number, start, end and sequence.
+    :rtype: namedtuple
+    """
+    fragment_length = int(dist.sample_truncated_gamma(
+        mean_length, gamma_shape, low_truncation, high_truncation))
+    upper_boundary = len(chromosome) - fragment_length
+    # Special case when upper boundary is less than the read length. Maybe
+    # should handle this by rejection?
+    if upper_boundary < fragment_length:
+        start = 0
+        end = len(chromosome)
+    else:
+        start = np.random.randint(0, upper_boundary)
+        end = start + fragment_length
+    fragment_sequence = chromosome.seq[start:end]
+    Fragment = namedtuple('Fragment', 'chrom uid start end seq')
+    return Fragment(chromosome.id, fragment_number, start, end, fragment_sequence)
+
+
+def simulate_fragments(chromosomes, mean_length, gamma_shape, low_truncation, high_truncation, number_fragments):
+    """Simulate a fragments from a set of chromosomes. Chromosomes are picked randomly for each fragment.
+
+    :param chromosomes: Chromosomes to simulate fragment from, a list of SeqRecord objects.
+    :param mean_length: Mean length of simulated fragments.
+    :param gamma_shape: Shape parameter of length distribution.
+    :param low_truncation: Minimum read length.
+    :param high_truncation: Maximum read length.
+    :param number_fragments: Number of fragments to simulate.
+    :returns: An iterator named tuples with chromosome id, fragment number, start, end and sequence.
+    :rtype: generator
+    """
+    for fragment_uid in xrange(number_fragments):
+        chromosome = sample_chromosome(chromosomes)
+        yield simulate_fragment(chromosome, mean_length, gamma_shape, low_truncation, high_truncation, fragment_uid)
