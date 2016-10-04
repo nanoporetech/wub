b15613b76e1494a29534f344a068246a92656cec
Botond Sipos <bsipos@nanoporetech.com>
2016-10-04 10:12:11 +0100

    Added tool to reverse fastq files.

---
+++ scripts/reverse_fastq.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+
+from wub.util import seq as seq_util
+from Bio import SeqIO
+
+# Parse command line arguments:
+parser = argparse.ArgumentParser(
+    description='Reverse sequences and qualities in fastq file.')
+parser.add_argument('input_fastq', nargs='?', help='Input fastq (default: stdin).',
+                    type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('output_fastq', nargs='?', help='Output fastq (default: stdout)',
+                    type=argparse.FileType('w'), default=sys.stdout)
+
+def reverse_seq_records(input_iterator):
+    """
+    """
+    for record in input_iterator:
+        print record
+        yield record[::-1]
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    input_iterator = seq_util.read_seq_records(args.input_fastq, format='fastq')
+    output_iterator = reverse_seq_records(input_iterator)
+    seq_util.write_seq_records(output_iterator, args.output_fastq, format='fastq')
