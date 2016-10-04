8534e0247610654d033cb036597645b76e6db7d9
Botond Sipos <bsipos@nanoporetech.com>
2016-10-04 09:57:48 +0100

    util.cmd: added wrapper function for finding executable.

---
+++ wub/util/cmd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Utilities related to running external commands."""
+
+from distutils.spawn import find_executable.
+
+def find_executable(command):
+    """Find executable in path corresponding to a command.
+
+    :param command: Command.
+    :returns: Path to executable of False.
+    :rtype: str
+    """
+    # In the future we might want to eliminate the dependency of
+    # distutils.
+    return find_executable(command)
