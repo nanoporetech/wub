05ac7fdd138a76213c99c9ce98b190f5c1cfa897
Botond Sipos <bsipos@nanoporetech.com>
2016-10-04 08:51:13 +0100

    util.seq: added functions to reverse complement DNA sequence without using biopython.

--- wub/util/seq.py
+++ wub/util/seq.py
@@ -10,9 +10,46 @@ from Bio.SeqRecord import SeqRecord
 """ Utilities manipulating biological sequences and formats. Extensions to biopython functionality.
 """
 
+# Reverse complements of bases, taken from dragonet:
+comp = {
+    'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'X': 'X', 'N': 'N',
+    'a': 't', 't': 'a', 'c': 'g', 'g': 'c', 'x': 'x', 'n': 'n',
+    '-': '-'
+}
+
 # Shortcut to the list of DNA bases:
 bases = sorted(list(IUPACUnambiguousDNA().letters))
 ambiguous_bases = sorted(list(IUPACAmbiguousDNA().letters))
+
+
+def com(k):
+    """ Return complement of base.
+
+    Performs the subsitutions: A<=>T, C<=>G, X=>X for both upper and lower
+    case. The return value is identical to the argument for all other values.
+
+    :param k: A base.
+    :returns: Complement of base.
+    :rtype: str
+
+    """
+    try:
+        return comp[k]
+    except KeyError:
+        sys.stderr.write(
+            "WARNING: No reverse complement for {} found, returning argument.".format(k))
+        return k
+
+
+def reverse_complement(seq):
+    """ Return reverse complement of a string (base) sequence.
+
+    :param seq: Input sequence.
+    :returns: Reverse complement of input sequence.
+    :rtype: str
+
+    """
+    return reduce(lambda x, y: x + y, map(com, seq[::-1]))
 
 
 def mock_qualities(record, mock_qual):
