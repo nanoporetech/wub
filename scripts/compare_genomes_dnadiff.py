1df4ca359f5ff8345af7c85f19bc401c4b2ce328
Botond Sipos <bsipos@nanoporetech.com>
2016-10-10 16:12:59 +0100

    compare_genomes_dnadiff: dealing with ouput and saving results.

--- scripts/compare_genomes_dnadiff.py
+++ scripts/compare_genomes_dnadiff.py
@@ -6,19 +6,24 @@ import argparse
 
 from wub.util import cmd as cmd_util
 from wub.wrappers import dnadiff
+from wub.util import misc
 
 # Parse command line arguments:
 parser = argparse.ArgumentParser(
     description="""Compare a set of reference sequences (genome) to another set (target assembly) using mummer's dnadiff.
     """)
 parser.add_argument(
+    '-p', metavar='results_pickle', type=str, help="Save pickled results in this file (None).", default=None)
+parser.add_argument(
+    '-r', metavar='raw_file', type=str, help="Save dnadiff report in this file (None).", default=None)
+parser.add_argument(
     '-d', metavar='work_dir', type=str, help="Use this working directory instead of a temporary directory (None).", default=None)
 parser.add_argument(
     '-k', action="store_true", help="Keep dnadiff result files (False).", default=False)
 parser.add_argument(
-    '-t', metavar='details_tsv', type=str, help="Save details of dnadiff comparison in this tab-separated file (None).", default=None)
+    '-v', action="store_true", help="Print out dnadiff output (False).", default=False)
 parser.add_argument(
-    '-r', metavar='report_pdf', type=str, help="Report with alignment details plot (None).", default=None)
+    '-t', metavar='details_tsv', type=str, help="Save details of dnadiff comparison in this tab-separated file (None).", default=None)
 parser.add_argument(
     'ref', metavar='reference_fasta', type=str, help="Reference fasta.")
 parser.add_argument(
@@ -34,4 +39,19 @@ if __name__ == '__main__':
     cmd_util.ensure_executable('show-coords')
     cmd_util.ensure_executable('nucmer')
 
-    print dnadiff.dnadiff(args.ref, args.target, args.d, not args.k)
+    results, raw_report, log = dnadiff.dnadiff(args.ref, args.target, args.d, not args.k)
+
+    if args.v:
+        sys.stdout.write(log)
+
+    if args.r is not None:
+        with open(args.r, 'w') as out_handle:
+            out_handle.write(raw_report)
+
+    if args.p is not None:
+        misc.pickle_dump(results, args.p)
+
+    for section, properties in results['Alignments'].iteritems():
+        print section, ":\t\tref\tquery"
+        for name, prop in properties.iteritems():
+            print "\t{}\t{}\t{}".format(name, prop.ref, prop.query)
