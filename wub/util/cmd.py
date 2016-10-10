27014348e244d1662e110023372bb833052c2f6f
Botond Sipos <bsipos@nanoporetech.com>
2016-10-10 10:20:24 +0100

    util.cmd: Added ensure_executable.

--- wub/util/cmd.py
+++ wub/util/cmd.py
@@ -2,6 +2,7 @@
 """Utilities related to running external commands."""
 
 from distutils.spawn import find_executable as exefind_distutils
+import sys
 
 
 def find_executable(command):
@@ -14,3 +15,16 @@ def find_executable(command):
     # In the future we might want to eliminate the dependency of
     # distutils.
     return exefind_distutils(command)
+
+def ensure_executable(command):
+    """Find executable in path corresponding to a command and abort if not found.
+
+    :param command: Command.
+    :returns: None
+    :rtype: object
+    """
+    # In the future we might want to eliminate the dependency of
+    # distutils.
+    if not find_executable(command):
+        sys.stderr.write("Required command \"{}\" not found in path! Aborting.!\n".format(command))
+        sys.exit(127)
