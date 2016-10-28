ab093355ab11e1d92f307c09f45d620829a70556
Botond Sipos <Botond.Sipos@nanoporetech.com>
2016-10-28 15:35:13 +0100

    bam.filter: handling the case of missing AS tag.

--- wub/bam/filter.py
+++ wub/bam/filter.py
@@ -4,6 +4,22 @@
 import itertools
 
 
+def get_alignment_score(segement):
+    """Get alignment score from pysam segment.
+
+    :param segment: Pysam aligned segment.
+    :returns: Alignment score.
+    :rtype: int
+    """
+
+    score = 0
+    try:
+        score = segement.get_tag('AS')
+    except:
+        pass
+    return score
+
+
 def filter_top_per_query(records_iter):
     """Filter pysam records keeping top scoring per query. Assumes
     records are sorted by name.
@@ -17,7 +33,7 @@ def filter_top_per_query(records_iter):
         if len(buff) == 0:
             buff.append(rec)
         elif rec is None or buff[-1].query_name != rec.query_name:
-            sorted_buff = sorted(buff, key=lambda x: x.get_tag('AS'), reverse=True)
+            sorted_buff = sorted(buff, key=get_alignment_score, reverse=True)
             buff = [rec]
             yield sorted_buff[0]
         else:
