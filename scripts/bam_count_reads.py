67f79098c64aba8c7510fc14273c6f53c73a979a
Botond Sipos <Botond.Sipos@nanoporetech.com>
2016-12-08 13:34:01 +0000

    Option to record individual base frequencies.

--- scripts/bam_count_reads.py
+++ scripts/bam_count_reads.py
@@ -5,7 +5,7 @@ import argparse
 import sys
 
 import pandas as pd
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 from wub.bam import read_counter
 from wub.util import misc
@@ -23,11 +23,11 @@ parser.add_argument(
 parser.add_argument(
     '-z', metavar='ref_fasta', type=str, help="Reference fasta. GC content and length columns are added if present (None).", default=None)
 parser.add_argument(
+    '-b', action="store_true", help="Include base frequencies in output (False).", default=False)
+parser.add_argument(
     '-p', metavar='results_pickle', type=str, help="Save pickled results in this file (None).", default=None)
 parser.add_argument(
     '-t', metavar='tsv_file', type=str, help="Save results in tsv format in this file (bam_count_reads.tsv).", default="bam_count_reads.tsv")
-parser.add_argument(
-    '-s', action="store_true", help="Sort results before saving in tsv format (False).", default=False)
 parser.add_argument(
     '-Q', action="store_true", help="Be quiet and do not print progress bar (False).", default=False)
 parser.add_argument(
@@ -38,14 +38,12 @@ if __name__ == '__main__':
 
     counts = read_counter.count_reads(
         args.bam, in_format=args.f, min_aln_qual=args.a, verbose=not args.Q)
-
-    if args.s:
-        counts = OrderedDict(counts.iteritems())
+    counts = OrderedDict(counts.iteritems())
 
     data = OrderedDict()
 
     if args.z is not None:
-        lengths, gc_contents = {}, {}
+        lengths, gc_contents, base_freqs = {}, {}, defaultdict(dict)
         ref_iter = seq_util.read_seq_records(args.z)
         if not args.Q:
             sys.stderr.write("Calculating sequence features:\n")
@@ -57,11 +55,24 @@ if __name__ == '__main__':
                 counts[ref.id] = 0
             lengths[ref.id] = len(ref)
             gc_contents[ref.id] = seq_util.gc_content(str(ref.seq))
+            if args.b:
+                bf = seq_util.base_composition(ref.seq)
+                for base, count in bf.iteritems():
+                    base_freqs[ref.id][base] = float(count) / len(ref)
+
         data['Length'] = [lengths[tr] for tr in counts.iterkeys()]
         data['GC'] = [gc_contents[tr] for tr in counts.iterkeys()]
 
     data['Reference'] = counts.keys()
     data['Count'] = counts.values()
+
+    if args.b and args.z:
+        for base in seq_util.bases:
+            tmp = []
+            for ref in counts.iterkeys():
+                tmp.append(base_freqs[ref][base])
+            data[base] = tmp
+
     data_frame = pd.DataFrame(data)
     data_frame = data_frame.sort(['Count'], ascending=False)
 
