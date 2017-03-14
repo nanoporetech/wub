a7b49e7d39937b7f47b0fbef634a8e35988e8335
Botond Sipos <Botond.Sipos@nanoporetech.com>
2017-03-14 10:54:11 +0000

    Fixed N50 test.

--- wub/tests/test_contig_stats.py
+++ wub/tests/test_contig_stats.py
@@ -1,6 +1,7 @@
 import unittest
 
 from wub.read_stats import contig_stats
+import pandas as pd
 
 
 class TestUtilSeq(unittest.TestCase):
@@ -9,5 +10,5 @@ class TestUtilSeq(unittest.TestCase):
 
     def test_N50(self):
         """Test calculation of N50."""
-        sequence_lengths = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.assertEqual(contig_stats.N50(sequence_lengths), 8.0)
+        sequence_lengths = pd.DataFrame({'dummy': [2, 3, 4, 5, 6, 7, 8, 9, 10]}
+        self.assertEqual(contig_stats.N50(sequence_lengths, 'dummy'), 8.0)
