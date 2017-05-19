cdea30c1822649fc0f0ed89757c4d5440da350a8
Botond Sipos <Botond.Sipos@nanoporetech.com>
2017-05-19 17:34:28 +0100

    docs building: working on fix.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ from unittest.mock import MagicMock
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-            return MagicMock()
+        return MagicMock()
 
 MOCK_MODULES = ['pysam']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
