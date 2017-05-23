a9e5333210c12880ee5968e5029bd9cdb80ad8d4
Botond Sipos <Botond.Sipos@nanoporetech.com>
2017-05-23 10:14:27 +0100

    docs: adding modules to requirements instead of mocking.

--- docs/conf.py
+++ docs/conf.py
@@ -16,15 +16,15 @@ import sys
 import os
 import imp
 import subprocess
-from mock import Mock as MagicMock
+# from mock import Mock as MagicMock
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
+# class Mock(MagicMock):
+#    @classmethod
+#    def __getattr__(cls, name):
+#        return MagicMock()
 
-MOCK_MODULES = ['pysam', 'tqdm', 'pandas', 'Bio', 'numpy', 'editdistance', '']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# MOCK_MODULES = ['pysam', 'tqdm', 'pandas', 'Bio', 'numpy', 'editdistance', '']
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
