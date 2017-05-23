536d64cd30dbb59f5bc7fc7d3654869259824690
Botond Sipos <Botond.Sipos@nanoporetech.com>
2017-05-23 09:55:29 +0100

    docs: fixes in cmd_tools.py.

--- docs/cmd_tools.py
+++ docs/cmd_tools.py
@@ -4,6 +4,7 @@
 have and parser object defined in their global scope - taken from tang
 
 """
+from __future__ import print_function
 import sys
 import os
 import imp
@@ -12,7 +13,7 @@ scripts_rel = 'scripts'
 attr_name = 'parser'
 blacklist = ['__init__.py']
 
-location = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
+location = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 scripts_abs = os.path.join(location, scripts_rel)
 scripts = sorted(filter(lambda s: s[0] != '.' and s not in blacklist, os.listdir(scripts_abs)))
 
@@ -21,33 +22,33 @@ sys.stderr.write("Found following scripts:\n{}\n{}\n{}\n".format(
 ))
 
 
-print """
+print ("""
 .. _command_line_tools:
 
 Command line tools
 ==================
-"""
+""")
 
 for script in scripts:
     script_name, script_ext = os.path.splitext(script)
     if script_ext == '.pyc':
         continue
-    
+
     try:
         mod_name = '{}.{}'.format(scripts_rel, script_name)
-        #mod = __import__(mod_name, globals(), locals(), [attr_name])
+        # mod = __import__(mod_name, globals(), locals(), [attr_name])
         mod = imp.load_source(script_name, os.path.join(scripts_abs, script))
         script = script.replace('.py', '')
 
-        print '.. _{}:\n\n{}\n{}'.format(script, script, '-'*len(script))
+        print ('.. _{}:\n\n{}\n{}'.format(script, script, '-' * len(script)))
         if hasattr(mod, attr_name):
-            print """
+            print ("""
 .. argparse::
    :ref: {}.{}
    :prog: {}
-""".format(mod_name, attr_name, script_name)
+""".format(mod_name, attr_name, script_name))
         else:
-            print 'No documentation available'
+            print ('No documentation available')
 
     except Exception as e:
         # Wha' yer' gonna do?
