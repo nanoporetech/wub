d4c574c8b3b9fb2aad1819c6694d3d2c1e06abc1
Botond Sipos <Botond.Sipos@nanoporetech.com>
2017-06-23 14:44:48 +0100

    Added blastn outfmt 6 parser.

---
+++ wub/parsers/blastn.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+""" Parser functions for blastn outfmt 6. """
+
+import six
+
+
+def _parse_coord_line(line):
+    """ Parse a line from a blast outfmt 6 file. """
+    fields = line.split()
+    aln_record = {
+        'query': fields[0],
+        'ref': fields[1],
+        'identity': float(fields[2]),
+        'aln_length': int(fields[3]),
+        'mismatch': int(fields[4]),
+        'gapopen': int(fields[5]),
+        'query_start': int(fields[6]),
+        'query_end': int(fields[7]),
+        'ref_start': int(fields[8]),
+        'ref_end': int(fields[9]),
+        'evalue': float(fields[10]),
+        'bitscore': float(fields[11]),
+        'strand': '+'
+    }
+
+    if aln_record['ref_start'] > aln_record['ref_end']:
+        aln_record['strand'] = '-'
+        aln_record['ref_start'], aln_record['ref_end'] = aln_record['ref_end'], aln_record['ref_start']
+    return aln_record
+
+def parse_coords(input_object):
+    """ Parse coordinates file produced by blastn outfmt 6.
+
+    :param input_object: Input path or file hanlder.
+    :returns: List of dictionaries with parsed records.
+    :rtype: list
+    """
+    if type(input_object) == str:
+        input_object = open(input_object, 'r')
+    records = []
+    for line in input_object:
+        line = line.strip()
+        records.append(_parse_coord_line(line))
+    return records
---
+++ wub/tests/test_blastn_coord_parse.py
@@ -0,0 +1,18 @@
+import unittest
+
+import six
+from os import path
+from collections import OrderedDict
+from Bio import SeqIO
+from wub.parsers import blastn
+
+
+class TestBlastnCoordParse(unittest.TestCase):
+
+
+    def test_nucmer_coord_parse(self):
+        """Test blastn outfmt 6 cooridnate parsing."""
+        top = path.dirname(__file__)
+        coord_file = path.join(top, "data/test_blastn_parse/blastn_test.coords")
+        records = blastn.parse_coords(coord_file)
+        self.assertEqual(records,  [{'gapopen': 0, 'query_end': 100, 'mismatch': 0, 'ref_end': 300, 'query': 'seq_0', 'identity': 100.0, 'bitscore': 181.0, 'query_start': 1, 'ref_start': 201, 'strand': '+', 'aln_length': 100, 'ref': 'seq_0', 'evalue': 3e-50}, {'gapopen': 0, 'query_end': 100, 'mismatch': 0, 'ref_end': 600, 'query': 'seq_0', 'identity': 100.0, 'bitscore': 181.0, 'query_start': 1, 'ref_start': 501, 'strand': '-', 'aln_length': 100, 'ref': 'seq_0', 'evalue': 3e-50}])
